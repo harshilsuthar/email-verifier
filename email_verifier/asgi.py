6a31bf2d18acbd77d3ecbc086d220c2295141eb1
Harshil Suthar <sharshil07@gmail.com>
2020-11-14 19:58:37 +0530

    change name from varifier to verifier

--- email_verifier/asgi.py
+++ email_verifier/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for email_varifier project.
+ASGI config for email_verifier project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_varifier.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_verifier.settings')
 
 application = get_asgi_application()
--- email_verifier/wsgi.py
+++ email_verifier/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for email_varifier project.
+WSGI config for email_verifier project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_varifier.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_verifier.settings')
 
 application = get_wsgi_application()
